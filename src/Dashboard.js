039289ae11c359a78826fb90792f48782e8c7627
Owcollier <owcollier@gmail.com>
2018-02-02 15:50:55 -0500

    Dashboard component rendering

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,18 +7,18 @@ export default function Dashboard(props) {
       <section className="section">
         <header className= "header">
           <h3>{props.catToAdopt.name}</h3>
-          <img src={props.catToAdopt.imageUrl} alt={props.catToAdopt.imageDescription}/>
+          <img src={props.catToAdopt.imageURL} alt={props.catToAdopt.imageDescription}/>
         </header>
         <main className="main">
           <dl className="dl">
-            <dt>Gender</dt>
+            <dt>Gender:</dt>
             <dd>{props.catToAdopt.sex}</dd>
-            <dt>Age</dt>
+            <dt>Age:</dt>
             <dd>{props.catToAdopt.age}</dd>
-            <dt>Breed</dt>
-            <dd>props.catToAdopt.breed</dd>
-            <dt>Story</dt>
-            <dd>props.catToAdopt.story</dd>
+            <dt>Breed:</dt>
+            <dd>{props.catToAdopt.breed}</dd>
+            <dt>Story:</dt>
+            <dd>{props.catToAdopt.story}</dd>
           </dl>
           <button className="button">Adopt this cat!</button>
         </main>
@@ -26,18 +26,18 @@ export default function Dashboard(props) {
       <section className="section">
         <header className= "header">
           <h3>{props.dogToAdopt.name}</h3>
-          <img src={props.dogToAdopt.imageUrl} alt={props.dogToAdopt.imageDescription}/>
+          <img src={props.dogToAdopt.imageURL} alt={props.dogToAdopt.imageDescription}/>
         </header>
         <main className="main">
           <dl className="dl">
-            <dt>Gender</dt>
+            <dt>Gender:</dt>
             <dd>{props.dogToAdopt.sex}</dd>
-            <dt>Age</dt>
+            <dt>Age:</dt>
             <dd>{props.dogToAdopt.age}</dd>
-            <dt>Breed</dt>
-            <dd>props.dogToAdopt.breed</dd>
-            <dt>Story</dt>
-            <dd>props.dogToAdopt.story</dd>
+            <dt>Breed:</dt>
+            <dd>{props.dogToAdopt.breed}</dd>
+            <dt>Story:</dt>
+            <dd>{props.dogToAdopt.story}</dd>
           </dl>
           <button className="button">Adopt this dog!</button>
         </main>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Dashboard from './Dashboard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const cats = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Mittens',
+    sex: 'Male',
+    age: 4,
+    breed: 'Bengal',
+    story: 'Owner lost in a wormhole incident'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Moon',
+    sex: 'Female',
+    age: 1,
+    breed: 'Bengal',
+    story: 'Brought to shelter'
+  }
+];
+
+const dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Donovan',
+    sex: 'Male',
+    age: 6,
+    breed: 'Golden Retriever',
+    story: 'Family moved'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Barbara',
+    sex: 'Female',
+    age: 1,
+    breed: 'Golden Retriever',
+    story: 'Found wandering the street'
+  }
+];
+
+const cat = cats[0];
+const dog = dogs[0];
+
+ReactDOM.render(<Dashboard catToAdopt={cat} dogToAdopt={dog} />, document.getElementById('root'));
